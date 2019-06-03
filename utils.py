477882b397d16d56408f96ebcc605f7c4e98b08c
Puneet Saini <puneet29saini@gmail.com>
2019-06-03 21:19:02 +0530

    Added normalized batch and fixed gram_matrix

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 
 def gram_matrix(tensor):
-    (b, ch, h, w) = tensor.size
+    (b, ch, h, w) = tensor.size()
     features = tensor.view(b, ch, h*w)
     features_t = features.transpose(1, 2)
     gram = features.bmm(features_t) / (ch*h*w)
@@ -18,3 +18,11 @@ def load_image(filename, size=None, scale=None):
         img = img.resize((int(img.size[0] / scale), int(img.size[1] / scale)),
                          Image.ANTIALIAS)
     return(img)
+
+
+def normalize_batch(batch):
+    # Imagenet Normalization
+    mean = batch.new_tensor([0.485, 0.456, 0.406]).view(-1, 1, 1)
+    std = batch.new_tensor([0.229, 0.224, 0.225]).view(-1, 1, 1)
+    batch = batch.div_(255.0)
+    return (batch - mean) / std
